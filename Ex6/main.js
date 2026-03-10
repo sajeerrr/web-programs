db8d3b584973dad55cec9f804fc5c7919f706b06
sajeer <2508@tkmce.ac.in>
2026-03-10 14:48:52 +0530

    ex 6

---
+++ Ex6/main.js
@@ -0,0 +1,18 @@
+function validateform() {
+
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let phone = document.getElementById("phone").value.trim();
+    let dob = document.getElementById("dob").value;
+    let address = document.getElementById("address").value.trim();
+    let password = document.getElementById("password").value;
+    let confirmpassword = document.getElementById("confirmpassword").value;
+
+    let emailpattern = /^[^ ]+@[^ }+\.[a-z]{2,3}$/;
+
+    //name validation
+    if(name === ""){
+        alert("Name cannot be empty");
+        return false;
+    }
+}
